9102b4dbe55d395163b902388cb5d0d4805c49bc
nisseknudsen <nisse.knudsen@gmail.com>
2020-04-11 22:08:21 +0200

    Code structure overhaul: less redundancy, more inheritance, typed variables; added semseg annotation style loading

--- python/pandaset/sequence.py
+++ python/pandaset/sequence.py
@@ -1,66 +1,82 @@
-from .utils import subdirectories
-from .sensors import Lidar
-from .sensors import Camera
+from typing import Dict
+
+from .annotations import Cuboids
+from .annotations import SemanticSegmentation
 from .meta import GPS
 from .meta import Timestamps
-from .annotations import Cuboids
+from .sensors import Camera
+from .sensors import Lidar
+from .utils import subdirectories
 
 
 class Sequence:
-    def __init__(self, directory):
-        self._directory = directory
-        self.lidar = None
-        self.camera = None
-        self.gps = None
-        self.timestamps = None
-        self.cuboids = None
+    def __init__(self, directory: str):
+        self._directory: str = directory
+        self.lidar: Lidar = None
+        self.camera: Dict[str, Camera] = None
+        self.gps: GPS = None
+        self.timestamps: Timestamps = None
+        self.cuboids: Cuboids = None
         self._load_data_structure()
 
-    def _load_data_structure(self):
+    def _load_data_structure(self) -> None:
         data_directories = subdirectories(self._directory)
 
         for dd in data_directories:
             if dd.endswith('lidar'):
                 self.lidar = Lidar(dd)
-            if dd.endswith('camera'):
+            elif dd.endswith('camera'):
                 self.camera = {}
                 camera_directories = subdirectories(dd)
                 for cd in camera_directories:
                     camera_name = cd.split('/')[-1]
                     self.camera[camera_name] = Camera(cd)
-            if dd.endswith('meta'):
+            elif dd.endswith('meta'):
                 self.gps = GPS(dd)
                 self.timestamps = Timestamps(dd)
-            if dd.endswith('annotations'):
+            elif dd.endswith('annotations'):
                 annotation_directories = subdirectories(dd)
                 for ad in annotation_directories:
                     if ad.endswith('cuboids'):
                         self.cuboids = Cuboids(ad)
+                    elif ad.endswith('semseg'):
+                        self.semseg = SemanticSegmentation(ad)
 
-    def load(self):
+    def load(self) -> 'Sequence':
         self.load_lidar()
         self.load_camera()
         self.load_gps()
         self.load_timestamps()
         self.load_cuboids()
+        self.load_semseg()
+        return self
 
-    def load_lidar(self):
+    def load_lidar(self) -> 'Sequence':
         self.lidar.load()
         return self
 
-    def load_camera(self):
+    def load_camera(self) -> 'Sequence':
         for cam in self.camera.values():
             cam.load()
         return self
 
-    def load_gps(self):
+    def load_gps(self) -> 'Sequence':
         self.gps.load()
         return self
 
-    def load_timestamps(self):
+    def load_timestamps(self) -> 'Sequence':
         self.timestamps.load()
         return self
 
-    def load_cuboids(self):
+    def load_cuboids(self) -> 'Sequence':
         self.cuboids.load()
         return self
+
+    def load_semseg(self) -> 'Sequence':
+        if self.semseg:
+            self.semseg.load()
+        return self
+
+
+if __name__ == '__main__':
+    pass
