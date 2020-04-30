488d3b999532953e6c2297d127ec8953ac25accd
xpc <xiaopengchuan_intern@hesaitech.com>
2020-04-30 20:04:49 +0800

    add pointcloud tutorial for pandaset

--- python/pandaset/geometry.py
+++ python/pandaset/geometry.py
@@ -50,5 +50,33 @@ def projection(lidar_points, camera_data, camera_pose, camera_intrinsics, filter
     return points2d_camera, points3d_camera, inliner_indices_arr
 
 
+def lidar_points_to_ego(points, lidar_pose):
+    lidar_pose_mat = _heading_position_to_mat(
+        lidar_pose['heading'], lidar_pose['position'])
+    transform_matrix = np.linalg.inv(lidar_pose_mat)
+    return (transform_matrix[:3, :3] @ points.T +  transform_matrix[:3, [3]]).T
+
+
+def center_box_to_corners(box):
+    pos_x, pos_y, pos_z, dim_x, dim_y, dim_z, yaw = box
+    half_dim_x, half_dim_y, half_dim_z = dim_x/2.0, dim_y/2.0, dim_z/2.0
+    corners = np.array([[half_dim_x, half_dim_y, -half_dim_z],
+                        [half_dim_x, -half_dim_y, -half_dim_z],
+                        [-half_dim_x, -half_dim_y, -half_dim_z],
+                        [-half_dim_x, half_dim_y, -half_dim_z],
+                        [half_dim_x, half_dim_y, half_dim_z],
+                        [half_dim_x, -half_dim_y, half_dim_z],
+                        [-half_dim_x, -half_dim_y, half_dim_z],
+                        [-half_dim_x, half_dim_y, half_dim_z]])
+    transform_matrix = np.array([
+        [np.cos(yaw), -np.sin(yaw), 0, pos_x],
+        [np.sin(yaw), np.cos(yaw), 0, pos_y],
+        [0, 0, 1.0, pos_z],
+        [0, 0, 0, 1.0],
+    ])
+    corners = (transform_matrix[:3, :3] @ corners.T + transform_matrix[:3, [3]]).T
+    return corners
+
+
 if __name__ == '__main__':
     pass
