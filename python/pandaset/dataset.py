02ea7b53978bf3734f598f3c2a530421826e0b96
Nisse Knudsen <nisse.knudsen@scaleapi.com>
2020-05-06 14:30:24 +0200

    Finished docstrings for dataset.py

--- python/pandaset/dataset.py
+++ python/pandaset/dataset.py
@@ -5,6 +5,19 @@ from .utils import subdirectories
 
 
 class DataSet:
+    """Top-level class to load PandaSet
+
+        ``DataSet`` prepares and loads ``Sequence`` objects for every sequence found in provided directory.
+        Access to a specific sequence is provided by using the sequence name as a key on the ``DataSet`` object.
+
+        Args:
+             directory: Absolute or relative path where PandaSet has been extracted to.
+
+        Examples:
+            >>> pandaset = DataSet('/data/pandaset')
+            >>> s = pandaset['002']
+        """
+
     def __init__(self, directory: str) -> None:
         self._directory: str = directory
         self._sequences: Dict[str, Sequence] = None
@@ -29,6 +42,18 @@ class DataSet:
             self._sequences[seq_id] = Sequence(sd)
 
     def sequences(self) -> List[str]:
+        """ Lists all available sequence names
+
+        Returns:
+            List of sequence names.
+
+        Examples:
+            >>> pandaset = DataSet('/data/pandaset')
+            >>> print(pandaset.sequences())
+            ['002','004','080']
+
+
+        """
         return list(self._sequences.keys())
 
 
